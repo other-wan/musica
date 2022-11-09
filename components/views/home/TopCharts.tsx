ee8966264b8076ee77e6f9cd9dddf01bb9056ee5
other-wan <muhammedridwan02@gmail.com>
2022-11-09 17:14:39 +0100

    test: scroll not working 2

--- components/views/home/TopCharts.tsx
+++ components/views/home/TopCharts.tsx
@@ -4,38 +4,11 @@ import TopChartCard from "./TopchartCard";
 const TopCharts = () => {
   return (
     <div className={TopChartStyles.topChart}>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-      tenetur tempora sunt porro! Veritatis facilis ipsum, sit ipsam eveniet
-      corporis molestiae, reprehenderit, ea necessitatibus fugit quos placeat
-      incidunt architecto nobis! Illo eligendi praesentium iure obcaecati atque
-      eius nihil, porro laborum ut facere non suscipit at ducimus est sapiente,
-      quos consectetur culpa ipsam temporibus fugiat! Soluta iure vero fugit
-      autem voluptates! Ea nesciunt possimus quis aspernatur laborum
-      perspiciatis totam commodi. In distinctio id quaerat odio consectetur
-      assumenda necessitatibus autem ex? Temporibus libero nam dignissimos!
-      Placeat iste magni atque? Iste, optio labore? Impedit earum atque debitis
-      deleniti at accusamus nisi necessitatibus id reiciendis facilis alias
-      totam, sint porro fuga quisquam optio animi voluptas, omnis ut officiis
-      ullam? Odio cupiditate dolorem repellendus aliquid. Architecto esse sed
-      maxime dolorem quasi, ipsum accusamus optio tempore eligendi, nulla facere
-      beatae placeat nesciunt harum dicta eos veniam? Earum eligendi vel eos
-      perspiciatis consectetur voluptates error. Magnam, dolore. Adipisci autem
-      reprehenderit praesentium incidunt explicabo possimus culpa ducimus dolor
-      voluptatum molestias magnam officia nulla facere a suscipit quod qui
-      quisquam, error voluptas nam iusto quibusdam tempore rem quam? Odio. Alias
-      cumque reprehenderit reiciendis nesciunt consequuntur numquam vero
-      placeat. Vero temporibus eum nesciunt. Mollitia, culpa ad alias porro
-      exercitationem, laboriosam, repudiandae adipisci totam sapiente vel beatae
-      explicabo sit fuga illo. Consequuntur sequi ipsam animi? Error
-      voluptatibus accusantium distinctio, explicabo excepturi ullam blanditiis,
-      expedita, laboriosam velit asperiores porro dolores officia accusamus
-      eveniet omnis eligendi natus pariatur repudiandae est. Laborum,
-      perspiciatis officia! Quidem harum numquam autem quos enim? Quae,
-      provident laborum illo eum doloremque dolor, maxime ullam atque beatae
-      cupiditate officia saepe quas et neque magni, quidem ut vero facilis
-      error. Eum. Cum animi voluptate, odit illo rem illum odio repellendus eius
-      eos temporibus corrupti dolorem facere voluptates similique id amet
-      ducimus culpa itaque delectus dignissimos ipsa quas vel aliquam modi. Ea.
+      <TopChartCard />
+      <TopChartCard />
+      <TopChartCard />
+      <TopChartCard />
+      <TopChartCard />
     </div>
   );
 };
